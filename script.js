3ffce52eaf5323326a7572e450610bbe80397ded
Krzysztof Kapcia <krzysztofkapcia@gmail.com>
2017-03-26 15:09:08 +0200

    poprawiono mozliwość badania parzystości liczb ujemnych

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 function parity(number) {
 
-  if(number == 0) {
+  if (number < 0) {     // dzięki temu zabiegowi będziemy mogli
+    number *= -1;       // badać liczby ujemne nie powodując błędu polegającego na  
+  }                     // nieskończonej ilości wywołań funkcji przez samą siebie
+
+
+  if (number == 0) {
     return true;
   }
   else if (number == 1) {
     return false;
   }
   else 
-    return parity(number - 2);
+    return parity(number - 2);    // tu tworzymy rekurencje korzystając z własności parzystości liczb
 }
 
-console.log(parity(6));
+console.log(parity(-15));         // tu wywołujemy funkcję sprawdzania parzystości liczby -15
